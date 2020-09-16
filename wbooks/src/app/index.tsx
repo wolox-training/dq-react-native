7209a253b5d9a059b5880d7a31062ebe42399e2e
Diego Quiros <diegoaquiros@gmail.com>
2020-09-16 18:35:15 -0300

    finish bookcell

---
+++ wbooks/src/app/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+import BookDetail from './components/BookDetail';
+
+const App = () => {
+  return (
+    <>
+    <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF"/>
+    <SafeAreaView>
+      <BookDetail title='A Little Bird Told Me' subtitle='Timothy Cross' url='https://reactnative.dev/img/tiny_logo.png'/>
+      <BookDetail title='When the Doves Disappeared' subtitle='Sofi Oksanen' url='https://reactnative.dev/img/tiny_logo.png'/>
+      <BookDetail title='The Best Book in the World' subtitle='Peter Sjernstrom' url='https://reactnative.dev/img/tiny_logo.png'/>
+    </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
