4d09adadc3d45eeb29365b69bbd9440704cb1496
Diego Quiros <diegoaquiros@gmail.com>
2020-09-29 12:16:56 -0300

    add status bar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '@screens/HomeScreen';
 import DetailScreen from '@screens/DetailScreen';
 import { DETAIL_SCREEN, HOME_SCREEN } from '@constants/routes';
+import { WHITE } from '@constants/colors';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor={WHITE} />
       <Stack.Navigator initialRouteName={HOME_SCREEN}>
         <Stack.Screen name={HOME_SCREEN} component={HomeScreen} />
         <Stack.Screen name={DETAIL_SCREEN} component={DetailScreen} />
