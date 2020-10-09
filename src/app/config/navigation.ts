4c198ae146e3abead0617bb21d99024d8a407960
Diego Quiros <diegoaquiros@gmail.com>
2020-10-09 10:59:35 -0300

    set margin on title style

--- src/app/config/navigation.ts
+++ src/app/config/navigation.ts
@@ -1,16 +1,16 @@
 import { WHITE } from '@constants/colors';
 import NavBackground from '@components/NavBackground';
-import { NAV_HEIGHT, NOTCH_SIZE } from '@constants/commonStyles';
+import { NAV_HEIGHT } from '@constants/commonStyles';
 
 export const stackNavigatorConfig = {
   headerTitleStyle: {
     fontSize: 16,
     color: WHITE,
     fontWeight: '600',
-    letterSpacing: -0.4
+    letterSpacing: -0.4,
+    marginBottom: 20
   },
   headerStyle: NAV_HEIGHT,
-  headerStatusBarHeight: 10 + NOTCH_SIZE,
   headerBackTitleVisible: false,
   headerBackground: NavBackground,
   headerTransparent: true,
