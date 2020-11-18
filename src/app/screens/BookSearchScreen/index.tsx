fdfa47557e628895fea7531102b4785d2ffa9a70
Diego Quiros <diegoaquiros@gmail.com>
2020-11-18 12:14:21 -0300

    reselect

--- src/app/screens/BookSearchScreen/index.tsx
+++ src/app/screens/BookSearchScreen/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { AppState } from '@interfaces/redux';
 import WithEmptyResult from '@components/WithEmptyResult';
 import WithEmptySearch from '@components/WithEmptySearch';
 import BookList from '@components/BookList';
 
+import { searchSelector, search } from './utils';
+
 function BookSearchScreen() {
-  const books = useSelector((state: AppState) => state.book.books);
-  const search = useSelector((state: AppState) => state.book.bookSearch);
-  const searchResult = books.filter(book => book.title.includes(search.toLowerCase()));
+  const searchWord = useSelector(search);
+  const searchResult = useSelector(searchSelector);
   return (
-    <WithEmptySearch isEmpty={search.length === 0}>
+    <WithEmptySearch isEmpty={searchWord.length === 0}>
       <WithEmptyResult isEmpty={searchResult.length === 0}>
         <BookList books={searchResult} />
       </WithEmptyResult>
